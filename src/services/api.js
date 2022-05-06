2f0526820235e90cac636f20039127b6db179d9d
rogelio1502 <rogelio15021515@gmail.com>
2022-05-06 15:37:17 -0500

    añadir_validar_datos_personales

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 const instance = axios.create({
-  // baseURL: "https://escuela-para-padres-api.herokuapp.com",
-  baseURL: "http://localhost:7777",
+  baseURL: "https://escuela-para-padres-api.herokuapp.com",
+  // baseURL: "http://localhost:7777",
   headers: {
     "Content-Type": "application/json",
   },
